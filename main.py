0479b5417da92fab57527a5f1e24d34a7949e21d
wahyuardi <wahyuardiismail@gmail.com>
2019-10-31 17:52:44 +0700

    penyelesaian issue #1

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ with sr.Microphone() as source:
     audio = r.listen(source)
     
     try:
-        print("YOU SAY :" + r.recognize_google(audio, language = 'id-ID'))#3
+        print("YOU SAY :" + r.recognize_google(audio, language = 'id-ID'))
     except Exception as e:
         print(e)
 
 print("TIME OVER, THANK YOU")
+#1
