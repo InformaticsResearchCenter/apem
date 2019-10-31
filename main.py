1c9dc028c098307acddf452969a3c5035d6b35fd
wahyuardi <wahyuardiismail@gmail.com>
2019-10-31 19:33:50 +0700

    penyelesaian issues #9

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import speech_recognition as sr 
 r=sr.Recognizer() 
-with sr.Microphone() as source: #penyelesaian issues #8
+with sr.Microphone() as source: 
     print("SPEAK ANYTHING")
     audio = r.listen(source)
     
     try:
-        print("YOU SAY :" + r.recognize_google(audio, language = 'id-ID'))
+        print("YOU SAY :" + r.recognize_google(audio, language = 'id-ID')) #penyelesaian issues #9
     except Exception as e:
         print(e)
 
