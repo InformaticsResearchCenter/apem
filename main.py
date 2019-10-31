be5cd9b02e75ee1c36158a09f70195a128f25c40
wahyuardi <wahyuardiismail@gmail.com>
2019-10-31 17:48:55 +0700

    penyelesaian issue #3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ with sr.Microphone() as source:
     audio = r.listen(source)
     
     try:
-        print("YOU SAY :" + r.recognize_google(audio, language = 'id-ID'))
+        print("YOU SAY :" + r.recognize_google(audio, language = 'id-ID'))#3
     except Exception as e:
         print(e)
 
