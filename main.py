acc59bc606373fb611fb7d3bf49e2cc0be3d853e
wahyuardi <wahyuardiismail@gmail.com>
2019-11-07 18:00:45 +0700

    ifhashsflsa #14

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import speech_recognition as sr
 r=sr.Recognizer() 
 with sr.Microphone() as source: 
     print("SPEAK ANYTHING")
-    audio = r.listen(source)
+    audio = r.listen(source) 
     
     try:
         print("YOU SAY :" + r.recognize_google(audio, language = 'id-ID')) 
     except Exception as e: 
         print(e) 
-
+    ifhashsflsa #14
 print("TIME OVER, THANK YOU") 
