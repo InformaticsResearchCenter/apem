35a92ec3a9acddb8290b1015aaadbe25eb6e4ea8
Dyning Aida <aida.batrishya@gmail.com>
2019-11-16 13:46:37 +0700

    penjelasan issue #21

---
+++ apem.py
@@ -0,0 +1,47 @@
+import speech_recognition as sr
+from selenium import webdriver 
+from selenium.webdriver.firefox.options import Options
+class Apem(object): #issue 21
+    def __init__(self, filname):
+        self.filname = filname
+
+    def masuk(self): 
+        self.opsi = Options() 
+        self.opsi = webdriver.firefox.options.Options() 
+        self.opsi.headless = False 
+        self.cap = webdriver.common.desired_capabilities.DesiredCapabilities().FIREFOX 
+        self.cap['marionette'] = True 
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://siap.poltekpos.ac.id")
+
+    def login(self): 
+        self.opsi = Options() 
+        self.opsi = webdriver.firefox.options.Options() 
+        self.opsi.headless = False
+        self.cap = webdriver.common.desired_capabilities.DesiredCapabilities().FIREFOX 
+        self.cap['marionette'] = True 
+        self.driver = webdriver.Firefox() 
+        self.driver.get("https://siap.poltekpos.ac.id")
+        self.driver.find_element_by_name('user_name').send_keys("1184030")
+        self.driver.find_element_by_name('user_pass').send_keys("sariasih54")
+        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[2]/div/form/input[4]').click()
+
+
+    def speak(self):
+        r= sr.Recognizer()
+        with sr.Microphone() as source:
+            print("SAY SOMETHING, PLEASE")
+            audio = r.listen(source)
+        try:
+            print("TEXT : "+r.recognize_google(audio, language='id-ID'))
+            x = "siap"
+            y = "login siap"
+            if (r.recognize_google(audio, language='id-ID')) == x:
+                self.masuk()
+            if (r.recognize_google(audio, language='id-ID')) == y:
+                self.login()
+        except Exception as e:
+            print(e)
+            print("error")
+ 
+        print("Time is over, thanks")
--- main.py
+++ main.py
@@ -1,41 +1,4 @@
-import speech_recognition as sr
-def masuk():
-        from selenium import webdriver 
-        from selenium.webdriver.firefox.options import Options 
-        opsi = Options() 
-        opsi = webdriver.firefox.options.Options() 
-        opsi.headless = False 
-        cap = webdriver.common.desired_capabilities.DesiredCapabilities().FIREFOX 
-        cap['marionette'] = True 
-        driver = webdriver.Firefox()
-        driver.get("https://siap.poltekpos.ac.id")
+import apem
 
-def login():
-        from selenium import webdriver
-        from selenium.webdriver.firefox.options import Options 
-        opsi = Options() 
-        opsi = webdriver.firefox.options.Options() 
-        opsi.headless = False
-        cap = webdriver.common.desired_capabilities.DesiredCapabilities().FIREFOX 
-        cap['marionette'] = True 
-        driver = webdriver.Firefox() 
-        driver.get("https://siap.poltekpos.ac.id")
-        driver.find_element_by_name('user_name').send_keys("1184030") #issue 20
-        driver.find_element_by_name('user_pass').send_keys("sariasih54")
-        driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[2]/div/form/input[4]').click()
-r= sr.Recognizer()
-with sr.Microphone() as source:
-    print("SAY SOMETHING, PLEASE")
-    audio = r.listen(source)
-try:
-    print("TEXT : "+r.recognize_google(audio, language='id-ID'))
-    x = "siap"
-    y = "login siap"
-    if (r.recognize_google(audio, language='id-ID')) == x:
-        masuk()
-    if (r.recognize_google(audio, language='id-ID')) == y:
-        login()
-except Exception as e:
-    pass
- 
-print("Time is over, thanks")
+speech = apem.Apem(object)
+speech.speak()
