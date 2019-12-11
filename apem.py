d9d959b43ec54d5c6443a33b1be7a72a605a181e
wahyuardi <wahyuardiismail@gmail.com>
2019-12-11 12:47:47 +0700

    penyelesaian issues #34

--- apem.py
+++ apem.py
@@ -19,8 +19,8 @@ class Apem(object):
         self.cap['marionette'] = True
         self.driver = webdriver.Firefox()
         self.driver.get('http://siap.poltekpos.ac.id/siap/besan.depan.php')
-        self.driver.find_element_by_name('user_name').send_keys(self.npm) #issues 33
-        self.driver.find_element_by_name('user_pass').send_keys(self.paswd)
+        self.driver.find_element_by_name('user_name').send_keys(self.npm) 
+        self.driver.find_element_by_name('user_pass').send_keys(self.paswd) #issues 34
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[2]/div/form/input[4]').click()
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/a[5]').click()
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[1]/table/tbody/tr/td[3]/select/option[5]').click()
