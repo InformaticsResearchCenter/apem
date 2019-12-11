44009b60d24d43c1d16291fd7451fef3dc0694bc
wahyuardi <wahyuardiismail@gmail.com>
2019-12-11 12:44:02 +0700

    penyelesaian issues #33

--- apem.py
+++ apem.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 class Apem(object): 
     def __init__(self, npm, paswd): 
-        self.npm = npm #issues 32
-        self.paswd = paswd #issues 32
+        self.npm = npm 
+        self.paswd = paswd 
     def masuk(self): 
         self.opsi = Options()
         self.opsi.headless = False
@@ -19,7 +19,7 @@ class Apem(object):
         self.cap['marionette'] = True
         self.driver = webdriver.Firefox()
         self.driver.get('http://siap.poltekpos.ac.id/siap/besan.depan.php')
-        self.driver.find_element_by_name('user_name').send_keys(self.npm)
+        self.driver.find_element_by_name('user_name').send_keys(self.npm) #issues 33
         self.driver.find_element_by_name('user_pass').send_keys(self.paswd)
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[2]/div/form/input[4]').click()
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/a[5]').click()
