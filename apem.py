53e51586e93abe91586da89af6bb1991d7cea07c
wahyuardi <wahyuardiismail@gmail.com>
2019-12-11 13:13:49 +0700

    penyelesaian issues #46

--- apem.py
+++ apem.py
@@ -2,9 +2,9 @@ import speech_recognition as sr
 from selenium import webdriver 
 from selenium.webdriver.firefox.options import Options
 class Apem(object): 
-    def __init__(self, npm, paswd): 
-        self.npm = npm 
-        self.paswd = paswd 
+    def __init__(self, npm, paswd):
+        self.npm = npm
+        self.paswd = paswd
     def masuk(self): 
         self.opsi = Options()
         self.opsi.headless = False
@@ -19,13 +19,13 @@ class Apem(object):
         self.cap['marionette'] = True
         self.driver = webdriver.Firefox()
         self.driver.get('http://siap.poltekpos.ac.id/siap/besan.depan.php')
-        self.driver.find_element_by_name('user_name').send_keys(self.npm) 
-        self.driver.find_element_by_name('user_pass').send_keys(self.paswd) 
+        self.driver.find_element_by_name('user_name').send_keys(self.npm)
+        self.driver.find_element_by_name('user_pass').send_keys(self.paswd)
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[2]/div/form/input[4]').click()
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[1]/table[2]/tbody/tr[1]/td[2]/a[5]').click()
         self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[3]/tbody/tr[1]/td[2]/p[1]/table/tbody/tr/td[3]/select/option[5]').click()
         self.driver.find_element_by_class_name('button').click()
-    def ceknilai2(self):
+    def ceknilai2(self): #issues 46
         self.opsi = Options()
         self.opsi.headless = False
         self.cap = webdriver.common.desired_capabilities.DesiredCapabilities().FIREFOX
@@ -73,7 +73,7 @@ class Apem(object):
         self.driver.get('http://siap.poltekpos.ac.id/siap/besan.depan.php')
         self.driver.find_element_by_name('user_name').send_keys(self.npm)
         self.driver.find_element_by_name('user_pass').send_keys(self.paswd)
-        self.driver.find_element_by_name('login').click() #issues  35
+        self.driver.find_element_by_xpath('/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td[2]/table[2]/tbody/tr[1]/td[2]/div/form/input[4]').click()
     def kalender(self):
         self.opsi = Options()
         self.opsi.headless = False
